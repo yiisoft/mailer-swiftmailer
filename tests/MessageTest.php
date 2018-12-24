36f8b5c135545740666c2f74b500a23809c141ca
Mehdi Achour <machour@gmail.com>
2018-12-24 11:51:57 +0100

    Remove useless TestCase.php, fix namespace

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -45,7 +45,7 @@ class MessageTest extends \yii\tests\TestCase
      */
     protected function getTestFilePath()
     {
-        return Yii::getAlias('@yiiunit/swiftmailer/runtime') . DIRECTORY_SEPARATOR . basename(get_class($this)) . '_' . getmypid();
+        return Yii::getAlias('@yii/swiftmailer/tests/runtime') . DIRECTORY_SEPARATOR . basename(get_class($this)) . '_' . getmypid();
     }
 
     /**
