fc4e94569589ce9fd020286f0f36b0c45b7fb85c
Razon Yang <razonyang@gmail.com>
2019-07-21 04:07:07 +0800

    Fix #1: Adjust to yiisoft/mailer's changes (#9)

--- src/Logger.php
+++ src/Logger.php
@@ -1,63 +1,26 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
+namespace Yiisoft\Mailer\SwiftMailer;
 
-namespace Yiisoft\Yii\SwiftMailer;
-
-use Psr\Log\LogLevel;
-use yii\helpers\Yii;
+use Psr\Log\{LoggerInterface, LogLevel};
 
 /**
- * Logger is a SwiftMailer plugin, which allows passing of the SwiftMailer internal logs to the
- * Yii logging mechanism. Each native SwiftMailer log message will be converted into Yii 'info' log entry.
- *
- * This logger will be automatically created and applied to underlying [[\Swift_Mailer]] instance, if [[Mailer::$enableSwiftMailerLogging]]
- * is enabled. For example:
- *
- * ```php
- * [
- *     'components' => [
- *         'mailer' => [
- *             '__class' => Yiisoft\Yii\SwiftMailer\Mailer::class,
- *             'enableSwiftMailerLogging' => true,
- *         ],
- *      ],
- *     // ...
- * ],
- * ```
- *
- *
- * In order to catch logs written by this class, you need to setup a log route for 'Yiisoft\Yii\SwiftMailer\Logger::add' category.
- * For example:
- *
- * ```php
- * [
- *     'components' => [
- *         'log' => [
- *             'targets' => [
- *                 [
- *                     '__class' => Yiisoft\Log\FileTarget::class,
- *                     'categories' => ['Yiisoft\Yii\SwiftMailer\Logger::add'],
- *                 ],
- *             ],
- *         ],
- *         // ...
- *     ],
- *     // ...
- * ],
- * ```
- *
- * @author Paul Klimov <klimov.paul@gmail.com>
- * @since 2.0.4
+ * Logger is a PSR-3 logger plugin for SwiftMailer.
  */
 class Logger implements \Swift_Plugins_Logger
 {
     /**
-     * {@inheritdoc}
+     * @var LoggerInterface $logger logger instance.
      */
+    private $psrLogger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $psrLogger)
+    {
+        $this->psrLogger = $psrLogger;
+    }
+
     public function add($entry)
     {
         $categoryPrefix = substr($entry, 0, 2);
@@ -76,20 +39,14 @@ class Logger implements \Swift_Plugins_Logger
                 $level = LogLevel::INFO;
         }
 
-        Yii::log($level, $entry, ['category' => __METHOD__]);
+        $this->psrLogger->log($level, $entry, ['category' => __METHOD__]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function clear()
     {
         // do nothing
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function dump()
     {
         return '';
---
+++ tests/TestTransport.php
@@ -0,0 +1,43 @@
+<?php
+namespace Yiisoft\Mailer\SwiftMailer\Tests;
+
+class TestTransport extends \Swift_Transport_EsmtpTransport
+{
+    /**
+     * @var Swift_Events_EventListener[] $plugins
+     */
+    public $plugins;
+
+    public function __construct()
+    {
+        call_user_func_array(
+            [$this, 'Swift_Transport_EsmtpTransport::__construct'],
+            \Swift_DependencyContainer::getInstance()->createDependenciesFor('transport.smtp')
+        );
+    }
+
+    public function registerPlugin(\Swift_Events_EventListener $plugin)
+    {
+        $this->plugins[] = $plugin;
+        parent::registerPlugin($plugin);
+    }
+
+    protected function getBufferParams()
+    {
+        return [];
+    }
+
+    public function send(\Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
+    {
+        if (empty($message->getSubject())) {
+            throw new \Swift_RfcComplianceException('Subject is required');
+        }
+
+        return 1;
+    }
+
+    public function isStarted()
+    {
+        return true;
+    }
+}
